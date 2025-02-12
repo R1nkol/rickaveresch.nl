cb61279fb2749aea07a1f545f007409e3e3f7382
R1nkol <rick.maisweg@gmail.com>
2025-02-12 23:10:13 +0100

    Removed Footer on game page

--- src/app/projects/cyberbox/game/page.js
+++ src/app/projects/cyberbox/game/page.js
@@ -1,11 +1,8 @@
 import Head from "next/head";
-import Header from "@/components/Header";
-import Footer from "@/components/Footer";
 
 export default function Game() {
   return (
     <div style={{ width: "100vw", height: "100vh", overflow: "hidden" }}>
-        {/* <Header /> */}
       <iframe
         src="/CyberBoxWebVersion/index.html"
         width="100%"
@@ -13,7 +10,6 @@ export default function Game() {
         style={{ border: "none" }}
         className="absolute inset-0 m-auto"
       />
-        <Footer />
     </div>
   );
 }
